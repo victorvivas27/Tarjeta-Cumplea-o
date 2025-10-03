b85d62328647337c42587ed52e40ecefd115b19f
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-10-03 11:56:36 -0300

    fix: Simplifique la configuración de dotenv eliminando la ruta personalizada para el archivo de entorno

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 import dotenv from "dotenv";
 
-dotenv.config({ path: process.env.ENV_FILE || ".env" });
+dotenv.config();
 export default defineConfig({
   e2e: {
        baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:5173",
