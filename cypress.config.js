16783617939a7bafa96e8746ae916c4e780a5de8
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-10-03 10:37:31 -0300

    refactor: eliminar el paso de empaquetado del proyecto del flujo de trabajo de Cypress y limpiar la configuración

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+     devServer:{
+        framework:"react",
+        bundler:"vite"
+      },
     setupNodeEvents(on, config) {
-     
-      // implement node event listeners here
+    
+      
     },
      baseUrl:"http://localhost:5173/"
   },
