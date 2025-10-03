c230c3f902ac003370b7ac7db734d4221fe264f9
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-10-03 11:29:45 -0300

    feat: Agregar soporte para variables de entorno y configurar Cypress para usar dotenv

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
 
+dotenv.config({ path: process.env.ENV_FILE || ".env" });
 export default defineConfig({
   e2e: {
-     devServer:{
-        framework:"react",
-        bundler:"vite"
-      },
-    setupNodeEvents(on, config) {
-    
-      
-    },
-     baseUrl:"http://localhost:4173/"
-  },
+       baseUrl: process.env.CYPRESS_BASE_URL || "http://localhost:5173",
+    setupNodeEvents(on, config) {},
+     },
 });
